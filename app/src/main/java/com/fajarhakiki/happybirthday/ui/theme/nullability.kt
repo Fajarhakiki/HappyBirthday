d7dede8a05dad60293a654f3ed0700a92733de61
Fajar <fajarhakiki1525@gmail.com>
2024-10-17 14:06:05 +0700

    first commit

---
+++ app/src/main/java/com/fajarhakiki/happybirthday/ui/theme/nullability.kt
@@ -0,0 +1,9 @@
+package com.fajarhakiki.happybirthday.ui.theme
+
+fun main() {
+    val favoriteActor: String? = "Sandra Oh"
+
+    val lengthOfName = favoriteActor?.length ?: 0
+
+    println("The number of characters in your favorite actor's name is $lengthOfName.")
+}
